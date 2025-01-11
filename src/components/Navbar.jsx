d9c522467b25a547b8767336e11425445a32d577
Sugini Injani <forggdev@gmail.com>
2025-01-11 21:32:54 +0700

    `Add data and change navbar`

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import NavLogo from '../img/logo-navbar.png';
 import Search from './Search';
-import Shopee from '../img/icons-shopee.png';
 
 function Navbar() {
   return (
@@ -17,7 +16,6 @@ function Navbar() {
                 <NavLink to='/kertas' className={({ isActive }) => isActive? 'hover: underline active font-bold': 'hover:underline'}>Kertas</NavLink>
                 <NavLink to='/peralatan' className={({ isActive }) => isActive? 'hover: underline active font-bold': 'hover:underline'}>Peralatan</NavLink>
                 <Search/>
-                <NavLink to='https://shopee.co.id/elsaatk'><img width="25" height="25" src={Shopee} alt="shopee"/></NavLink>
                 <NavLink to='https://maps.app.goo.gl/UyTpzhtbTNxCySai8'><i className="fa-solid fa-map-location-dot fa-xl text-primary-blue"></i></NavLink>
             </nav>
         </div>
